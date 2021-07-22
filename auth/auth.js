ea099ddbdc6e756a357a515faccf8b92fe50a036
Kayode Ogunamkinwa <khizzyjr@gmail.com>
2021-07-22 09:35:39 -0400

    fixed access and refresh token bug

--- auth/auth.js
+++ auth/auth.js
@@ -1,9 +1,8 @@
 const jwt = require("jsonwebtoken");
-const tokens = require("../middleware/tokens");
 
 async function Auth(req, res, next) {
   try {
-    const { accessToken, refreshToken } = req.cookies;
+    const { accessToken } = req.cookies;
     if (!accessToken) return res.status(401).send("Please Login");
 
     const verified = jwt.verify(accessToken, process.env.JWT_SECRET);
@@ -12,34 +11,6 @@ async function Auth(req, res, next) {
     req.user = verified.user;
     next(); //use this method to keep moving.
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      try {
-        const { refreshToken } = req.cookies;
-        if (!refreshToken) return res.status(400).send("Please Login");
-
-        const verifiedRefresh = jwt.verify(
-          refreshToken,
-          process.env.JWT_SECRET
-        );
-        //generate a new access token
-        const { accessToken } = await tokens(verifiedRefresh.user);
-
-        res.status(200).cookie("accessToken", accessToken, {
-          expiresIn: "10m",
-          httpOnly: true,
-          path: "/",
-          sameSite: "none",
-          secure: true,
-        });
-
-        const verified = jwt.verify(accessToken, process.env.JWT_SECRET);
-        req.user = verified.user;
-
-        return next();
-      } catch (error) {
-        return res.status(401).send("Please Login");
-      }
-    }
     return res.status(401).send("Please Login");
   }
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -186,44 +186,46 @@ router.post("/login", async (req, res) => {
 //check if the user is logged in
 router.get("/loggedin", async (req, res) => {
   try {
-    const token = req.cookies.accessToken;
-    if (!token) {
+    const { accessToken } = req.cookies;
+    if (!accessToken) {
       return res.send(false);
     }
     //verify the token
-    jwt.verify(token, process.env.JWT_SECRET);
+    jwt.verify(accessToken, process.env.JWT_SECRET);
     res.send(true);
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      try {
-        const { refreshToken } = req.cookies;
-        if (!refreshToken) return res.status(400).send("Please Login");
-
-        const verifiedRefresh = jwt.verify(
-          refreshToken,
-          process.env.JWT_SECRET
-        );
-        //generate a new access token
-        const { accessToken } = await tokens(verifiedRefresh.user);
-
-        res.status(200).cookie("accessToken", accessToken, {
-          maxAge: 300000, // 5 minutes
-          httpOnly: true,
-          path: "/",
-          sameSite: "none",
-          secure: true,
-        });
-
-        const verified = jwt.verify(accessToken, process.env.JWT_SECRET);
-        req.user = verified.user;
-
-        return res.send(true);
-      } catch (error) {
-        return res.send(false);
-      }
-    }
-
     return res.send(false);
+  }
+});
+
+//periodically check if the user is logged in then generate a new access token.
+router.post("/refresh", async (req, res) => {
+  try {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      return res.status(400).send("Please Login");
+    }
+    //verify the token
+    const verified = jwt.verify(refreshToken, process.env.JWT_SECRET);
+    
+    console.log(verified);
+    if (verified.exp * 1000 <= Date.now()) {
+      return res.status(400).send("Your token has expired");
+    }
+    const { accessToken } = await tokens(verified.user);
+
+    res
+      .status(200)
+      .cookie("accessToken", accessToken, {
+        maxAge: 300000, // 5 minutes
+        httpOnly: true,
+        path: "/",
+        sameSite: "none",
+        secure: true,
+      })
+      .send();
+  } catch (error) {
+    return res.status(500).send(error.message);
   }
 });
 
